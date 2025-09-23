13c2ca9991b46c6ee8e9992619e4273b0fb6f5a3
jonashstudio <jonashibarra@gmail.com>
2025-09-23 17:03:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const copyMsg = document.getElementById("copyMsg");
 
 copyBtn.addEventListener("click", () => {
   navigator.clipboard.writeText(scriptCode.textContent).then(() => {
+    copyBtn.classList.add("copied");
     copyMsg.textContent = "Copied!";
-    setTimeout(() => copyMsg.textContent = "", 2000);
+    setTimeout(() => {
+      copyMsg.textContent = "";
+      copyBtn.classList.remove("copied");
+    }, 1500);
   }).catch(err => {
     copyMsg.textContent = "Failed to copy";
   });
